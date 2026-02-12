eeb4ff9411c22eb77e2c095d342c5a1516d3ad95
Ayrton Vargas <ayrtonvargas17@gmail.com>
2026-02-12 20:01:34 -0300

    Variables y ngModel

--- my-proyect/src/app/app.component.ts
+++ my-proyect/src/app/app.component.ts
@@ -1,14 +1,19 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 import { HeaderComponent } from './components/header/header.component';
+import { FormsModule } from '@angular/forms';
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet, HeaderComponent],
+  imports: [RouterOutlet, HeaderComponent, FormsModule],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
 export class AppComponent {
   title = 'my-proyect';
   userName: string = 'Adrian';
+  number: number = 0;
+  addOne() {
+    this.number++;
+  }
 }
